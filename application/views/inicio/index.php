a017d7b972267e812f7582db2f660a3d67e70af5
julio ramon <44277182+julio200815@users.noreply.github.com>
2021-02-10 15:26:18 -0600

    0.0.8 ajuste de carrousel pick up- promociones

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -126,9 +126,9 @@
 			</div>
 			<div id="carousel-pick-up" class="carousel slide carousel-multi-item" data-ride="carousel">
 				<ol class="carousel-indicators">
-					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					<li data-target="#carousel-pick-up" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-pick-up" data-slide-to="1"></li>
+					<li data-target="#carousel-pick-up" data-slide-to="2"></li>
 				</ol>
 				<div class="carousel-inner" role="listbox">
 					<div class="carousel-item active">
@@ -180,11 +180,11 @@
 						</div>
 					</div>
 				</div>
-				<a class="carousel-control-prev" href="#carousel-example-generic" role="button" data-slide="prev">
+				<a class="carousel-control-prev" href="#carousel-pick-up" role="button" data-slide="prev">
 					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 					<span class="sr-only">Previous</span>
 				</a>
-				<a class="carousel-control-next" href="#carousel-example-generic" role="button" data-slide="next">
+				<a class="carousel-control-next" href="#carousel-pick-up" role="button" data-slide="next">
 					<span class="carousel-control-next-icon" aria-hidden="true"></span>
 					<span class="sr-only">Next</span>
 				</a>
@@ -217,9 +217,9 @@
 			</div>
 			<div id="carousel-promos" class="carousel slide carousel-multi-item" data-ride="carousel">
 				<ol class="carousel-indicators">
-					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					<li data-target="#carousel-promos" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-promos" data-slide-to="1"></li>
+					<li data-target="#carousel-promos" data-slide-to="2"></li>
 				</ol>
 				<div class="carousel-inner" role="listbox">
 					<div class="carousel-item active">
@@ -271,11 +271,11 @@
 						</div>
 					</div>
 				</div>
-				<a class="carousel-control-prev" href="#carousel-example-generic" role="button" data-slide="prev">
+				<a class="carousel-control-prev" href="#carousel-promos" role="button" data-slide="prev">
 					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 					<span class="sr-only">Previous</span>
 				</a>
-				<a class="carousel-control-next" href="#carousel-example-generic" role="button" data-slide="next">
+				<a class="carousel-control-next" href="#carousel-promos" role="button" data-slide="next">
 					<span class="carousel-control-next-icon" aria-hidden="true"></span>
 					<span class="sr-only">Next</span>
 				</a>
