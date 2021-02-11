8d0bb5d556777392411ad257985c5bc4f1191700
julio ramon <44277182+julio200815@users.noreply.github.com>
2021-02-11 14:28:35 -0600

    0.0.8 Ajuste banner inicio

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -1,59 +1,64 @@
 
-
-<div class="row bg-banner-index">
-	<div class="col">
-			<div style="margin-top:160px;" class="text-center white">
-				<h1 class="card-title white" style="font-family:montserrat; font-weight:700; font-size:3em;">¡QUÉ BUENO ES TENERTE DE REGRESO!</h1>
-				<p style="font-family:montserrat; font-size:2em;">Shoppping, restaurantes, entretenimiento y más</p>
-			</div>
-
-			<style>
-				.optionPredetermined {
-					color: gray;
-				}
-				.optionGroup {
-					font-weight: bold;
-				}
-				.optionChild {
-				}
-
-				.auto_list {
-					position: absolute;
-				}
-
-				.carousel-inner img {
-				margin: auto;
-				}
-			</style>
-
-			<div class="row mb-3 mt-2">
-				<div class="offset-md-3 col-md-6 col-sm-12">
-					<fieldset>
-						<div class="input-group">
-
-							<div class="input-group-prepend">
-								<input type="text" class="form-control" name="buscador" id="buscador" autocomplete="off" placeholder="Buscar..." aria-describedby="buscador">
-							</div>
-
-							<select class="form-control" name="categorias_select" id="categorias_select" onchange="cambiar_seleccion(this)">
-								<option value="" class="optionPredetermined">Seleccione una categoría...</option>
-								<?php foreach ($categorias_list as $categoria_row): ?>
-										<option value="<?php echo $categoria_row->id; ?>" class="optionGroup"><?php echo $categoria_row->nombre; ?></option>
-										<?php foreach ($subcategorias_list as $subcategoria_row): ?>
-											<?php if ($subcategoria_row->subcategoria_de == $categoria_row->id): ?>
-												<option value="<?php echo $subcategoria_row->id; ?>" class="optionChild"><?php echo $subcategoria_row->nombre; ?></option>
-											<?php endif; ?>
-										<?php endforeach; ?>
-								<?php endforeach; ?>
-							</select>
-
-							<div class="input-group-append">
-								<button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
-							</div>
-
+<div class="app-content center-layout">
+	<div class="container-fluid">
+		<div class="content-body">
+			<div class="row bg-banner-index">
+				<div class="col">
+						<div style="margin-top:160px;" class="text-center white">
+							<h1 class="card-title white" style="font-family:montserrat; font-weight:700; font-size:3em;">¡QUÉ BUENO ES TENERTE DE REGRESO!</h1>
+							<p style="font-family:montserrat; font-size:2em;">Shoppping, restaurantes, entretenimiento y más</p>
 						</div>
-						<div id="resultado" class="list-group"></div>
-					</fieldset>
+
+						<style>
+							.optionPredetermined {
+								color: gray;
+							}
+							.optionGroup {
+								font-weight: bold;
+							}
+							.optionChild {
+							}
+
+							.auto_list {
+								position: absolute;
+							}
+
+							.carousel-inner img {
+							margin: auto;
+							}
+						</style>
+
+						<div class="row mb-3 mt-2">
+							<div class="offset-md-3 col-md-6 col-sm-12">
+								<fieldset>
+									<div class="input-group">
+
+										<div class="input-group-prepend">
+											<input type="text" class="form-control" name="buscador" id="buscador" autocomplete="off" placeholder="Buscar..." aria-describedby="buscador">
+										</div>
+
+										<select class="form-control" name="categorias_select" id="categorias_select" onchange="cambiar_seleccion(this)">
+											<option value="" class="optionPredetermined">Seleccione una categoría...</option>
+											<?php foreach ($categorias_list as $categoria_row): ?>
+													<option value="<?php echo $categoria_row->id; ?>" class="optionGroup"><?php echo $categoria_row->nombre; ?></option>
+													<?php foreach ($subcategorias_list as $subcategoria_row): ?>
+														<?php if ($subcategoria_row->subcategoria_de == $categoria_row->id): ?>
+															<option value="<?php echo $subcategoria_row->id; ?>" class="optionChild"><?php echo $subcategoria_row->nombre; ?></option>
+														<?php endif; ?>
+													<?php endforeach; ?>
+											<?php endforeach; ?>
+										</select>
+
+										<div class="input-group-append">
+											<button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+										</div>
+
+									</div>
+									<div id="resultado" class="list-group"></div>
+								</fieldset>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
