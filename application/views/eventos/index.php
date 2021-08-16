d2b3d963faaffe0913a32342408ca448586c6469
Eduardo GQ <lalogqs@gmail.com>
2021-08-16 17:31:15 -0500

    1.1.2 Cambios de la pagina de eventos

--- application/views/eventos/index.php
+++ application/views/eventos/index.php
@@ -3,10 +3,40 @@
 <div class="app-content container center-layout mt-2">
     <div class="content-wrapper">
         <div class="content-body">
-            <!-- INICIA CONTENIDO  -->
+            <div class="mt-2">
+                <div class="row mt-2">
+                    <div class="col-md-12 order-md-1">
+                        <div class="p-2 bg-white text-center">
 
-            <img src="almacenamiento\eventos\contenido-eventos.jpg" alt="" width="100%">
+                            <h1 class="text-uppercase purple"><strong>Disfruta nuestros eventos<br> y promociones en familia</strong></h1>
+                            <br>
+                            <p>
+                                En el centro comercial Solesta podrás disfrutar de diferentes atracciones
+                                <br>
+                                para toda la familia.
+                                <br>
+                                <br>
+                                Contamos con las medidas necesarias para cuidar tu salud y la de tu familia
+                                <br>
+                                ya que contamos con espacios abiertos para minimizar los riesgos.
+                            </p>
 
+                            <div class="row mt-3">
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="<?php echo base_url()."almacenamiento/eventos/evento-01.png"; ?>" alt="">
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="<?php echo base_url()."almacenamiento/eventos/evento-02.png"; ?>" alt="">
+                                </div>
+                                <div class="col-md-4">
+                                    <img class="img-fluid" src="<?php echo base_url()."almacenamiento/eventos/evento-03.png"; ?>" alt="">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
