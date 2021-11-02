d20138844b36ec5cf0f08b8590838e116948c5c2
julio ramon <44277182+julio200815@users.noreply.github.com>
2021-11-02 13:26:17 -0600

    1.1.4 Se oculto el banner de viaje esperando autorizacion

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -16,6 +16,7 @@
 			</div>
 		</div>
 
+		<!--
 		<div class="carousel-item">
 			<div class="row text-center">
 				<div class="col-md-12">
@@ -23,8 +24,9 @@
 					<a href="<?php echo base_url(); ?>categorias/ver/mercado_solesta"><img src="almacenamiento\inicio\banner-index-02-movil.jpg" alt="" width="100%" class="d-block d-sm-none img-flui"></a>
 				</div>
 			</div>
-		</div>
+		</div>-->
 
+		
 		<div class="carousel-item">
 			<div class="row text-center">
 				<div class="col-md-12">
