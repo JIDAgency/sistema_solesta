51ac3c66edb6409d3d62cac7623fb29c58fd42d8
marco-29 <126988064+marco-29@users.noreply.github.com>
2024-10-22 16:28:14 -0600

    1.5.2 Section explora movil

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -34,7 +34,7 @@
                         Puebla, te ofrecemos un centro comercial <br> moderno y familiar con una amplia gama de <br>
                         tiendas, restaurantes y entretenimiento para <br> todas las edades.</p>
 
-                    <div class="row">
+                    <!-- <div class="row">
                         <div class="col-5 d-none d-sm-block mt-11">
                             <button type="button" class="btn btn-llegar2 px-5 py-1">Cómo llegar</button>
                         </div>
@@ -46,17 +46,43 @@
                                 <a href="#"><img src="assets/images/redes/instagram.png" alt="instagram" class="img-fluid" width="14%"></a>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <div class="col-lg-5 col-md-5 col-sm-12">
                 <img src="assets/images/recursos/locales.png" alt="locales" class="img-fluid mt-5">
 
-                <div class="row mt-20">
+                <!-- <div class="row mt-20">
                     <div class="col-12 d-none d-sm-block text-center">
                         <button type="button" class="btn btn-locales px-5 py-1">Explorar todos los locales</button>
                     </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="d-none d-sm-block">
+    <div class="container mt-11 mb-12">
+        <div class="row">
+            <div class="row col-6">
+                <div class="col-6">
+                    <button type="button" class="btn btn-llegar2 px-5 py-2 float-end">Cómo llegar</button>
                 </div>
+
+                <div class="col-6">
+                    <div class="social-icons-movil2 text-center">
+                        <span class="text-white me-2">Síguenos: </span>
+                        <a href="#"><img src="assets/images/redes/tiktok.png" alt="tiktok" class="img-fluid" width="13%"></a>
+                        <a href="#"><img src="assets/images/redes/facebook.png" alt="facebook" class="img-fluid" width="13%"></a>
+                        <a href="#"><img src="assets/images/redes/instagram.png" alt="instagram" class="img-fluid" width="13%"></a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-6 d-none d-sm-block text-center">
+                <button type="button" class="btn btn-locales px-5 py-1">Explorar todos los locales</button>
             </div>
         </div>
     </div>
@@ -65,38 +91,47 @@
 <section class="explora">
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="row col-lg-5 col-md-5 col-sm-12">
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">RESTAURANTES</button>
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">Ver mas</button>
+            <div class="row col-lg-5 col-md-5 col-sm-12 mt-negativo">
+                <div class="col-5 me-2 px-3 pb-5 restaurante">
+                    <h2 class="marys">RESTAU <br>RANTES</h2>
+                    <a href="" class="text-white">Ver mas</a>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">DIVERSION</button>
+                <div class="col-5 me-2 px-3 pb-5 diversion">
+                    <h2 class="marys">DIVER <br>SION</h2>
+                    <a href="" class="text-white">Ver mas</a>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">WELLNESS</button>
+                <div class="col-5 me-2 px-3 pb-5 wellness">
+                    <h2 class="marys">WELL <br>NESS</h2>
+                    <a href="" class="text-white">Ver mas</a>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">ACCESORIOS</button>
+                <div class="col-5 me-2 px-3 pb-5 accesorios">
+                    <h2 class="marys">ACCE <br>SORIOS</h2>
+                    <a href="" class="text-white">Ver mas</a>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">SERVICIOS</button>
+                <div class="col-5 me-2 px-3 pb-5 servicios">
+                    <h2 class="marys">SERVI <br>CIOS</h2>
+                    <a href="" class="text-white">Ver mas</a>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">SHOPPING</button>
+                <div class="col-5 me-2 px-3 pb-5 shopping">
+                    <h2 class="marys">SHOPP <br>ING</h2>
+                    <a href="" class="text-white">Ver mas</a>
                 </div>
-                <div class="col-6">
-                    <button type="button" class="btn btn-llegar2 px-5 py-1">HOGAR</button>
+                <div class="col-5 me-2 px-3 pb-5 hogar">
+                    <h2 class="marys">HO <br>GAR</h2>
+                    <a href="" class="text-white">Ver mas</a>
+                </div>
+                <div class="col-12 text-center">
+                    <button type="button" class="btn btn-locales2 px-5 py-1">Explorar todos los locales</button>
                 </div>
             </div>
-            <div class="col-lg-5 col-md-5 col-sm-12">
-                <img src="assets/images/recursos/explora.png" alt="locales" class="img-fluid mt-5">
+            <div class="col-lg-5 col-md-5 col-sm-12 mt-negativo d-none d-sm-block">
+                <img src="assets/images/recursos/explora.png" alt="locales" class="img-fluid mb-3">
 
-                <div class="row mt-20">
-                    <div class="col-12 d-none d-sm-block text-center">
-                        <button type="button" class="btn btn-locales px-5 py-1">Explorar todos los locales</button>
-                    </div>
-                </div>
+                <h2>Explora Solesta</h2>
+                <br>
+                <p>Con una arquitectura abierta y moderna, <br> Solesta te invita a recorrer sus amplios <br>
+                    espacios llenos de luz. Desde tiendas <br> exclusivas hasta áreas de entretenimiento, <br>
+                    cada rincón está diseñado para brindar una <br> experiencia inolvidable a toda la familia</p>
             </div>
         </div>
     </div>
