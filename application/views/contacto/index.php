bdc7dd4fdbb8c23eafd0b23ac784975702d13d60
marco-29 <126988064+marco-29@users.noreply.github.com>
2024-10-24 17:05:14 -0600

    1.6.15 Vista contacto

--- application/views/contacto/index.php
+++ application/views/contacto/index.php
@@ -1,11 +1,20 @@
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 mb-5">
-                <h1 class="text-contacto d-none d-sm-block">CONTAC <br>TANOS</h1>
-                <h1 class="text-contacto-movil d-block d-sm-none">CONTAC <br>TANOS</h1>
+            <div class="col-lg-12 col-md-12 col-sm-12 mb-3">
+                <img src="<?php echo base_url('assets/images/recursos/banner-contacto.png') ?>" alt="banner" class="img-fluid d-none d-sm-block">
+                <img src="<?php echo base_url('assets/images/recursos/banner-contacto-movil.png') ?>" alt="banner" class="img-fluid d-block d-sm-none">
+                <!-- <h1 class="text-contacto d-none d-sm-block">CONTAC <br>TANOS</h1> -->
+                <!-- <h1 class="text-contacto-movil d-block d-sm-none">CONTAC <br>TANOS</h1> -->
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12">
+                <div class="row">
+                    <div class="col-12 mb-5">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.6678443789233!2d-98.231855424685!3d19.034352682161686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc730cc904b2b%3A0xb530f1deb31d3a3b!2sSolesta!5e0!3m2!1ses-419!2smx!4v1729639047944!5m2!1ses-419!2smx" width="100%" height="450" style="border: 0; border-radius: 7px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-5">
                 <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
                 <script>
                     hbspt.forms.create({
@@ -13,16 +22,6 @@
                         formId: "b3a21af8-db4e-48ce-a8b3-13e67f0c87e0"
                     });
                 </script>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <div class="row">
-                    <div class="col-12 mb-3">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.6678443789233!2d-98.231855424685!3d19.034352682161686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc730cc904b2b%3A0xb530f1deb31d3a3b!2sSolesta!5e0!3m2!1ses-419!2smx!4v1729639047944!5m2!1ses-419!2smx" width="100%" height="450" style="border: 0; border-radius: 7px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                    <div class="col-12">
-                        <p>Atlixcáyotl 4931, Reserva Territorial Atlixcáyotl, Centros Comerciales Desarrollo Atlixcayotl, 72193 Heroica Puebla de Zaragoza, Pue.</p>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
